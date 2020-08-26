574fa7a9b01774feeb71df7691a644164c32b842
queirozdaniel <danielqueirozak77@gmail.com>
2020-08-26 00:58:16 -0300

    Permitindo que aplicação seja observada pelo SpringBootAdmin

--- src/main/java/com/danielqueiroz/forum/config/security/SecurityConfig.java
+++ src/main/java/com/danielqueiroz/forum/config/security/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated()
 			.and().csrf().disable()
